212b911d0238640a6deb00423b104d913f2ce0d4
justinmillerKM <justin_miller@kindermorgan.com>
2020-02-11 14:20:57 -0600

    Commit

--- ModbusTest/Properties/AssemblyInfo.cs
+++ ModbusTest/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52a86e0d-b933-42a6-b5b9-e0b01562134b")]
+[assembly: Guid("e06d2e92-0117-414a-b285-00884fa518e1")]
 
 // Version information for an assembly consists of the following four values:
 //
